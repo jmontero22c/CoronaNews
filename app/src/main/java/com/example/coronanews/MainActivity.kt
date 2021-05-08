67f4a119bf48839c641d06124e7a5f8f06313249
jmontero22c <79382419+jmontero22c@users.noreply.github.com>
2021-05-08 11:01:14 -0500

    Inital Commit

--- app/src/main/java/com/example/coronanews/MainActivity.kt
+++ app/src/main/java/com/example/coronanews/MainActivity.kt
@@ -16,27 +16,29 @@ class MainActivity : AppCompatActivity() {
         val adapterSpinner: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this,R.array.Opciones,android.R.layout.simple_spinner_item)
         spinner.adapter=adapterSpinner*/
 
-        val spinner: Spinner = findViewById(R.id.spinner)
+        /*val spinner: Spinner = findViewById(R.id.spinner)
         ArrayAdapter.createFromResource(this, R.array.Opciones, android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
-        }
+        } */
 
         val noticias_list =findViewById<ListView>(R.id.noticias)
+        val noticiasGuardadas = findViewById<ListView>(R.id.noticias)
         val noticias = arrayListOf<noticias>(
-                noticias("El aislamiento de Brasil en la lucha contra el Covid-19", "n_1"),
-                noticias("Los países con más mortalidad por covid-19 en América", "n_2"),
-                noticias("Pacientes que tomaron antihipertensivos tienen menor riesgo de hospitalización o muerte por covid-19, según estudio", "n_3"),
-                noticias("Estos son los animales en los que OMS busca el origen del covid", "n_4"),
-                noticias("Alemania planea ampliar la producción de vacunas contra COVID-19", "n_5"),
-                noticias("La OMS recomienda usar ivermectina para el COVID-19 solo en ensayos clínicos", "n_6"),
-                noticias("Brasil detecta una nueva variante del covid-19 similar a la detectada en sudafricana", "n_7"),
-                noticias("Paciente con COVID-19 mata a otro porque el ruido del respirador le molestaba", "n_8")
+                noticias("India bate récord mundial de casos de coronavirus en 24 horas", "n_1"),
+                noticias("Coronavirus: por qué se han \"disparado\" las muertes de personas más jóvenes en América Latina", "n_2"),
+                noticias("Coronavirus: por qué es tan difícil desarrollar fármacos para tratar un virus como el de la covid-19", "n_3"),
+                noticias("Coronavirus en India: cómo la crisis por la pandemia puede afectar la economía mundial", "n_4"),
+                noticias("OMS aprobó uso de emergencia de una vacuna china contra covid-19 por primera vez", "n_5"),
+                noticias("¿Cuál es la diferencia entre cepa, variante y linaje del COVID-19?", "n_6"),
+                noticias("Pfizer/BioNTech producirán 4.000 millones de dosis de vacuna contra covid en 2022", "n_7"),
+                noticias("Desafíos Globales | La crisis del covid-19 en la India está fuera de control", "n_8")
         )
 
         val myadapter =noticiasAdapter(this,R.layout.item_noticia, noticias)
         noticias_list.adapter=myadapter
+
 
         noticias_list.setOnItemClickListener { parent, view, position, id ->
             goPage(view,position)
@@ -54,14 +56,14 @@ class MainActivity : AppCompatActivity() {
     fun goPage(view: View, position: Int) {
         var url:String = ""
         when(position){
-            0 -> url = "https://www.elespectador.com/noticias/el-mundo/el-aislamiento-de-brasil-en-la-lucha-contra-el-covid-19/"
-            1 -> url = "https://cnnespanol.cnn.com/video/mortalidad-covid-america-estados-unidos-coronavirus-perspectivas-buenos-aires/"
-            2 -> url = "https://www.semana.com/vida-moderna/articulo/pacientes-que-tomaron-antihipertensivos-tienen-menor-riesgo-de-hospitalizacion-o-muerte-por-covid-19-segun-estudio/202122/"
-            3 -> url = "https://www.eltiempo.com/vida/ciencia/los-animales-en-los-que-oms-busca-el-origen-del-covid-19-577054"
-            4 -> url = "https://www.dw.com/es/alemania-planea-ampliar-la-producci%C3%B3n-de-vacunas-contra-covid-19/a-57086610"
-            5 -> url = "https://news.un.org/es/story/2021/03/1490322"
-            6 -> url = "https://www.larepublica.co/globoeconomia/brasil-detecta-una-nueva-variante-del-covid-19-similar-a-la-detectada-en-sudafricana-3147477"
-            7 -> url = "https://www.wradio.com.co/noticias/internacional/paciente-con-covid19-mata-a-otro-porque-el-ruido-del-respirador-le-molestaba/20210408/nota/4124223.aspx"
+            0 -> url = "https://www.telesurtv.net/news/india-bate-record-mundial-casos-diarios-covid-20210507-0004.html"
+            1 -> url = "https://www.bbc.com/mundo/noticias-57010631"
+            2 -> url = "https://www.bbc.com/mundo/noticias-56945251"
+            3 -> url = "https://www.bbc.com/mundo/noticias-56943832"
+            4 -> url = "https://www.larepublica.co/globoeconomia/oms-aprueba-uso-de-emergencia-de-una-vacuna-china-contra-covid-19-por-primera-vez-3166238"
+            5 -> url = "https://bogota.gov.co/mi-ciudad/salud/diferencia-entre-cepa-variante-y-linaje-del-covid-19"
+            6 -> url = "https://www.larepublica.co/globoeconomia/pfizerbiontech-produciran-4000-millones-de-dosis-de-vacuna-contra-covid-en-2022-3166583"
+            7 -> url = "https://cnnespanol.cnn.com/2021/05/07/desafios-globales-covid-19-india-jose-levy-podcast-orix/"
         }
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(intent)
